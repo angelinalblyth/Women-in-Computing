80bc71364f2bea22f029a633b915761b877eb8fd
Angelina Blyth <angelinalblyth@hotmail.com>
2018-08-16 17:47:03 +0100

    Added Mary Lou Jepsen

--- API/db/seed.js
+++ API/db/seed.js
@@ -574,44 +574,53 @@ db.timeline_women.insert([
     wiki:"https://en.wikipedia.org/wiki/Ellen_Spertus"
   },
   {
-    name:"",
-    year:,
-    dob:,
-    dod:,
-    info:"",
-    contribution:"",
-    image:"",
-    wiki:""
+    name:"Jeri Ellsworth",
+    year:2004,
+    dob:1974,
+    dod:null,
+    info:"is an American entrepreneur and an autodidact computer chip designer and inventor. She gained fame in 2004 for creating a complete Commodore 64 system on a chip housed within a joystick, called C64 Direct-to-TV. That 'computer in a joystick' could run 30 video games from the early 1980's, and at peak, sold over 70,000 units in a single day via the QVC shopping channel.
+    Ellsworth co-founded CastAR (formerly Technical Illusions) in 2013 and stayed with the company until it shut down on June 26, 2017.In late 2014, she moved from Seattle to Mountain View, California with the CastAR team.",
+    contribution:"is a self-taught computer chip designer and creator of the C64 Direct-to-TV",
+    image:"https://upload.wikimedia.org/wikipedia/commons/7/7f/You%27re_one_baaad_cat.jpg",
+    wiki:"https://en.wikipedia.org/wiki/Jeri_Ellsworth"
   },
   {
-    name:"",
-    year:,
-    dob:,
-    dod:,
-    info:"",
-    contribution:"",
-    image:"",
-    wiki:""
+    name:"Lucy Sanders",
+    year:2004,
+    dob:1954,
+    dod:null,
+    info:"is the current CEO and a co-founder of the National Center for Women & Information Technology.[1] She is the recipient of many distinguished honors in the STEM fields, including induction into the US News STEM Leadership Hall of Fame in 2013.",
+    contribution:"co-founded the National Center for Women & Information Technology",
+    image:"https://upload.wikimedia.org/wikipedia/commons/4/47/Lucy_Sanders.jpg",
+    wiki:"https://en.wikipedia.org/wiki/Lucy_Sanders"
   },
   {
-    name:"",
-    year:,
-    dob:,
-    dod:,
-    info:"",
-    contribution:"",
-    image:"",
-    wiki:""
+    name:"Audrey Tang",
+    year:2005,
+    dob:1981,
+    dod:null,
+    info:"is a Taiwanese free software programmer, who has been described as one of the 'ten greats of Taiwanese computing.'In August 2016, she was invited to join the Taiwan Executive Yuan as a minister without portfolio, making her the first transgender official in the top executive cabinet.",
+    contribution:"was the initiator and leader of the Pugs project.",
+    image:"https://upload.wikimedia.org/wikipedia/commons/e/e5/Audrey_Tang_in_2015_%28cropped%29.jpg",
+    wiki:"https://en.wikipedia.org/wiki/Audrey_Tang"
   },
   {
-    name:"",
-    year:,
-    dob:,
-    dod:,
-    info:"",
-    contribution:"",
-    image:"",
-    wiki:""
+    name:"Mary Lou Jepsen",
+    year:2005,
+    dob:1965,
+    dod:null,
+    info:"is a technical executive and inventor in the fields of display, imaging, and computer hardware. She is the founder of OpenWater,[3] a startup working on fMRI-type imaging of the body using holographic, infrared techniques.[4] In 2016, she joined the board of Lear Corporation, a Detroit-area maker of seats and electronics for cars.
+
+She was previously an executive at Facebook / Oculus VR.Among her objectives at Facebook was leading a concerted effort to bring Virtual Reality to the next level. Previously she was Head of the Display Division at Google X where she led more than one 'Moon Shot' program, reported to Sergey Brin, and advised and directed display and consumer electronic programs throughout Google. The Wall Street Journal reported that among her projects there she created Google Lego TV: displays composed of smaller screens that plug together like Legos to create vast, seamless images and 'live walls' for wall size interaction, television, video conferencing and gaming, to virtual reality without having to wear anything on your face or body.
+
+Jepsen previously founded Pixel Qi in Taipei, Taiwan, focused on innovative opto-electronic architectures and the manufacturer thereof. Pixel Qi aimed to deliver high performance, low-power, sunlight-readable screens for mobile devices. The long term vision of Pixel Qi was to create devices that never need be recharged through a two prong approach 1) lowering the power consumption of the device through innovative screen and power management technology and 2) using alternative power generation and battery technologies.
+
+She was the co-founder and first chief technology officer of One Laptop per Child (OLPC) a non-profit that generated more than $1B in revenue and helped create $30B in revenue for its for-profit partners. There she designed the $100 laptop with new screen architectures, new very low power management architecture, new battery technology, new mesh networking protocol, and new user interface, and shipped millions of them to children in the developing world.
+
+In 2008 she was named to the Time 100, and In 2013 she was named to CNN's top 10 thinkers in science and technology, for her work in rethinking functional brain imaging.She is also a member of the Xconomists, an ad hoc team of editorial advisors for the tech news and media company, Xconomy.",
+    contribution:"was the founder and chief technology officer of One Laptop Per Child (OLPC), and the founder of Pixel Qi.",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Mary_Lou_Jepsen_in_Lab.jpg/1280px-Mary_Lou_Jepsen_in_Lab.jpg",
+    wiki:"https://en.wikipedia.org/wiki/Mary_Lou_Jepsen"
   },
   {
     name:"",
