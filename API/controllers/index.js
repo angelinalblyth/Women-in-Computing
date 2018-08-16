d0d8db5563950199413edfeb320f6c71ddf782c4
cjmcd123 <cj_mcdowall@btinternet.com>
2018-08-16 15:49:27 +0100

    working all timeline route

---
+++ API/controllers/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// router.use('/timeline', require('./timeline.js'));
+
+router.get('/', function (req, res) {
+  res.json({"data": "Timeine API"});
+});
+
+
+
+module.exports = router;
