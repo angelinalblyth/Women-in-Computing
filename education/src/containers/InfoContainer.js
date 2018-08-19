cb54c70c8436ee4075b747738e47eec60895fab8
cjmcd123 <cj_mcdowall@btinternet.com>
2018-08-19 21:40:02 +0100

    clicking image links to wiki

--- education/src/containers/InfoContainer.js
+++ education/src/containers/InfoContainer.js
@@ -2,17 +2,23 @@ import React from 'react';
 import InnerComponent from '../components/InnerComponent.js';
 import ImageComponent from '../components/ImageComponent.js';
 import InfoComponent from '../components/InfoComponent.js';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./InfoContainer.css";
 
 const InfoContainer = (props) => {
   let image = props.image;
+
+  const redirect = () => {
+    window.location.href = props.wiki;
+  }
+
   return(
       // <React.Fragment>
       //   <InnerComponent/>
       // </React.Fragment>
       <div className="info_tile" id={props.index}>
         <div className="info_container">
-          <div className="image_container" style={{backgroundImage: `url(${props.image})` }}>
+          <div className="image_container" style={{backgroundImage: `url(${props.image})` }} onClick={redirect}>
             <div className="bottom-left">
               <p><strong>{props.name}</strong></p>
               <p><strong>{props.year}</strong></p>
