e42c295b1fcb84b9c2ac68ad455b90b46e04781a
Angelina Blyth <angelinalblyth@hotmail.com>
2018-08-20 13:43:36 +0100

    Fixed footer to the bottom of the page

--- education/src/containers/MainContainer.js
+++ education/src/containers/MainContainer.js
@@ -4,6 +4,7 @@ import Header from '../components/Header.js';
 import Footer from '../components/Footer.js';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "../style/App.css";
+import "../style/Button.css";
 
 class MainContainer extends React.Component {
   constructor(props){
