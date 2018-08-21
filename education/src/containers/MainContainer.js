b030738a008963f721b82718d14975cc2f4d8b4c
user <user@users-MacBook-Pro.local>
2018-08-21 09:55:40 +0100

    Added redirect

--- education/src/containers/MainContainer.js
+++ education/src/containers/MainContainer.js
@@ -6,7 +6,7 @@ import Footer from '../components/Footer.js';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "../style/App.css";
 import "../style/Button.css";
-// import { Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import _ from "lodash";
 
 
@@ -95,7 +95,8 @@ class MainContainer extends React.Component {
           <React.Fragment>
             <Header/>
             <Route exact path="/" render= {({match}) =>
-              <TimeLineContainer women={this.state.women} match={match}/>
+            //<TimeLineContainer women={this.state.women} match={match}/>
+            <Redirect push to="/timeline/0" />
             }/>
             {/* <Route path="/timeline/year/:year" render = {({match}) =>
               <YearContainer women={this.state.women} match={match}/>
